4ff4eb29778926315323ad394ea1ab3e72393201
chenkexi <kexi.chen@androidmov.com>
2021-04-30 14:27:24 +0800

    config

--- config-server/src/main/java/com/amt/configserver/controller/ConfigTestController.java
+++ config-server/src/main/java/com/amt/configserver/controller/ConfigTestController.java
@@ -1,18 +1,30 @@
-package com.amt.configserver.controller;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * TODO
- *
- * @author ckx
- * @date: 2021/4/30 14:16
- */
-@RestController
-public class ConfigTestController {
-
-    @Value("${ribbon.NFLoadBalancerRuleClassName}")
-    String ribbonType;
-
-}
+//package com.amt.configserver.controller;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * TODO
+// *
+// * @author ckx
+// * @date: 2021/4/30 14:16
+// */
+//@RestController
+//public class ConfigTestController {
+//
+//    @Value("${ribbon.NFLoadBalancerRuleClassName}")
+//    String ribbonType;
+//
+//    @Value("${ribbon.ConnectTimeout}")
+//    String ribbonConnectTimeout;
+//
+//    @Value("${ribbon.OkToRetryOnAllOperations}")
+//    String ribbonOkToRetryOnAllOperations;
+//
+//
+//    @GetMapping("/getRibbonYml")
+//    public String getRibbonYml() {
+//        return ribbonType + ":" + ribbonConnectTimeout + ":" + ribbonType;
+//    }
+//}
